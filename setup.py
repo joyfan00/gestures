9c64e630cdc5a21965375c7023e460b5dc10167c
joyfan00 <93224590+joyfan00@users.noreply.github.com>
2023-11-24 18:11:36 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['gestures'],
+    package_dir={'': 'src'},
+    requires=["rospy", "geometry_msgs", "std_msgs", "baxter_core_msgs", "dynamic_reconfigure", "baxter_examples"]
+)
+
+setup(**d)
